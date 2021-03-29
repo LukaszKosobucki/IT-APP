2e270c3efe57b247c4b5a89e5036a61bc0d0b4ff
Karolina Bożek <kar.bozek@gmail.com>
2021-03-29 22:26:00 +0200

    Add firebase config to app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store/config";
 import withAuth from "./hoc/withAuth";
 import HomePage from "./stacks/HomeStack/HomePage";
+import "./firebase/config";
 
 function App() {
   return (
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyDLvABjM_Si2iyuKpO5ddJvewpYv-igK0Y",
+  authDomain: "sports-event-app.firebaseapp.com",
+  projectId: "sports-event-app",
+  storageBucket: "sports-event-app.appspot.com",
+  messagingSenderId: "286493677384",
+  appId: "1:286493677384:web:6011238b5e11ebcec1457d",
+  measurementId: "G-PYX2MPPC5K",
+};
+
+export default firebase.initializeApp(config);
