11184e994faa9bba0be4b65196f75179ba9ef29f
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2021-05-22 14:59:57 +0200

    about page layout

--- src/stacks/AboutStack/AboutPage.jsx
+++ src/stacks/AboutStack/AboutPage.jsx
@@ -1,3 +1,24 @@
-const AboutPage = () => <div>About</div>;
+import React, { Component } from "react";
+import styles from "./AboutPage.module.css";
+import { Button } from "../../components/shared/Buttons/Buttons";
+import { SIGN_UP } from "../../constants/paths";
+class AboutPage extends Component {
+
+    goSignUp = () => {
+        this.props.history.push(SIGN_UP)
+    }
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <p>
+                    this app is about this and that
+                </p>
+                <Button onClick={this.goSignUp} >Sign up</Button>
+            </div>
+
+        )
+    }
+}
 
 export default AboutPage;
