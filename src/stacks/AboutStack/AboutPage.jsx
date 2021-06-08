f2c4cf8c92e4c2de8d2db9e1dc01a5aa2ccf3f0d
Pawel <pawelwojtkowiak96@gmail.com>
2021-06-08 23:22:52 +0200

    changed about description

--- src/stacks/AboutStack/AboutPage.jsx
+++ src/stacks/AboutStack/AboutPage.jsx
@@ -16,21 +16,25 @@ class AboutPage extends Component {
                     <h2>Our team</h2>
                     <h3>Members:</h3>
                     <ul>
-                        <li>Jakub Majda</li>
                         <li>Karolina Bożek</li>
                         <li>Luís Sousa Rêgo</li>
-                        <li>Łukasz Brzeszcz</li>
                         <li>Łukasz Kosobucki</li>
-                        <li>ŁPaweł Wojtkowiak</li>
+                        <li>Paweł Wojtkowiak</li>
                         <li>Mikołaj Gromadzik</li>
                     </ul>
                     <h3></h3>
 
                 </div>
                 <div>
-                    <h2>Our app</h2>
+                    <h2>Sport Event Application</h2>
                     <p>
-                        this app is about this and that
+                        Application is meant to help with organization of any of the types of sport events.
+                        There are many sports available and you can select any you want.
+                        One of most important functionalities is auto genereting of a event ladder.
+                        Users can register as trainers, sportsmans and organizatiors.
+                        Each of them has different features available - starting with managing your own profie through managing your team and finishing with managing whole sport event!
+                        Do not waste more time or energy! Let the application help you.
+                        Try it now!
                 </p>
                 </div>
 
